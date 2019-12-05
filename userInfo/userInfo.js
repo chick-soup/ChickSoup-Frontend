3c829772c88398ee3680a7144b800b661ac95864
이성진 <chlskatnr1@naver.com>
2019-12-05 22:11:49 +0900

    [UPDATE] userInfo img base64 encoding 기능 추가

--- userInfo/userInfo.js
+++ userInfo/userInfo.js
@@ -1,3 +1,4 @@
+const server = "http://chicksoup.pythonanywhere.com";
 const userInfo = {
     "nickname": document.querySelector("#userInfo_input_nickname"),
     "img": document.querySelector("#userInfo_profileimage_input_label > img"),
@@ -9,49 +10,60 @@ function readURL(input) {
         let reader = new FileReader();
         reader.onload = (e) => userInfo.img.setAttribute("src", e.target.result);
         reader.readAsDataURL(input.files[0]);
-        console.log(input.files[0]);
     }
 }
 
+function encodeImageFileAsURL(element) {
+    let file = element.files[0];
+    let reader = new FileReader();
+    reader.onloadend = (e) => {
+        userInfo.img.setAttribute("src", e.target.result);
+        localStorage.setItem("img_base64", reader.result.split(",")[1]);
+    }
+    reader.readAsDataURL(file);
+}
+
 function signup() {
-    const url = "user/signup/profile";
+    const url = "/user/signup/profile";
     const data = {
         "nickname": userInfo.nickname.value.trim(),
-        "status_message": ""
+        "img_base64": localStorage.getItem("img_base64"),
     };
-    axiosPost(url, data).then((datas) => {
-
+    if(!checkNicknameLength())
+        return;
+    axiosPostWithToken(url, data).then((datas) => {
+        location.href = "../login/login.html";
     }).catch((error) => {
-
+        userInfo.errorText[0].innerHTML = "오류가 발생하였습니다.\n잠시만 기다려주세요.";
     })
 }
 
-function axiosPost(url, data) {
+function axiosPostWithToken(url, data) {
     return axios({
         method: "POST",
         url: `${server}${url}`,
         data: data,
+        headers: {
+            "Authorization": localStorage.getItem("access_token"),
+        }
     })
 }
 
 function checkNicknameLength(len) {
     let errorText = userInfo.errorText[0];
-    if(len < 3) {
+    if (len < 3 || len > 9) {
         setTextDisplay(errorText, "inline");
-        errorText.innerHTML = "닉네임은 최소 3자입니다.";
-    } else if(len > 9) {
-        setTextDisplay(errorText, "inline");
-        errorText.innerHTML = "닉네임은 최대 12자입니다.";
-    } else {
-        setTextDisplay(errorText, "none");
+        errorText.innerHTML = "닉네임은 최소 3자 최대 12자입니다.";
+        return false;
     }
+    setTextDisplay(errorText, "none");
+    return true;
 }
 
 function setTextDisplay(el, dis) {
     el.style.display = dis;
 }
 
-userInfo.imgInput.addEventListener("change", function () {
-    readURL(this);
-});
-
+window.onload = () => {
+    localStorage.removeItem("img_base64");
+}
