83b4943a6897e11d953e54c7d02abe1e661d6540
이성진 <chlskatnr1@naver.com>
2019-12-09 16:57:59 +0900

    Auto stash before merge of "feature/search_friend2" and "feature/userInfo"

--- friendList/friendList.js
+++ friendList/friendList.js
@@ -1,6 +1,9 @@
 const friendList = {
     "otherprofile": document.querySelector("#friendList_otherprofile"),
     "pageTitle": document.querySelector("#friendList_seemyfriend > h1"),
+    "block": document.querySelector("#friendList_blockfriend"),
+    "hide": document.querySelector("#friendList_hidefriend"),
+
 };
 const friendListObj = [
     {
@@ -31,80 +34,10 @@ const friendListObj = [
         "phone": "010-2222-4444",
         "block": false,
         "hide": false,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "이성진",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": false,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "김어진",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": false,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "이지수",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": false,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
-    }, {
-        "profileImg": "face.svg",
-        "bookmark": true,
-        "name": "박진홍",
-        "phone": "010-2222-4444",
-        "block": true,
-        "hide": true,
     },
 ];
 
-function friendFrame(profileImg, bookmark, name, phone) {
+const friendFrame = (profileImg, bookmark, name, phone) => {
     let frame =
         `<li class="friendList_profile_list">
             <img src="../img/${profileImg}" alt="profileImage">
@@ -136,9 +69,9 @@ function friendFrame(profileImg, bookmark, name, phone) {
             </div>
         </li>`;
     return frame;
-}
+};
 
-function friendFrameTwo(profileImg, name, phone, title) {
+const friendFrameTwo = (profileImg, name, phone, title) => {
     let frame =
         `<li class="friendList_profile_list">
             <img src="../img/${profileImg}" alt="profileImage">
@@ -161,9 +94,52 @@ function friendFrameTwo(profileImg, name, phone, title) {
             </div>
         </li>`;
     return frame;
-}
+};
 
-function makeBookmarkFriendList(list) {
+const removeOtherprofileInnerHTML = () => {
+    friendList.otherprofile.innerHTML = "";
+};
+
+const defaultDetails = () => {
+    friendList.detailImg.forEach((el) => {
+        el.parentNode.childNodes[3].classList.remove("showDetails");
+    })
+};
+
+const defaultStar = () => {
+    let star = document.querySelector("#friendList_bookmark");
+    star.setAttribute("src", "../img/star.svg");
+};
+
+const setPageTitle = (title) => {
+    friendList.pageTitle.innerHTML = title;
+};
+
+const IsHided = (data) => {
+    if (data.hide)
+        return true;
+    return false;
+};
+
+const IsBlocked = (data) => {
+    if (data.block)
+        return true;
+    return false;
+};
+
+const IsOnBookmark = (data) => {
+    if (data.bookmark)
+        return true;
+    return false;
+};
+
+const IsOnDetails = (el) => {
+    if (el.parentNode.childNodes[3].classList[1])
+        return true;
+    return false;
+};
+
+const makeBookmarkFriendList = (list) => {
     removeOtherprofileInnerHTML();
     setPageTitle("내 친구 보기");
     friendListObj.filter(IsOnBookmark).map((datas) => {
@@ -172,35 +148,37 @@ function makeBookmarkFriendList(list) {
             friendFrame(datas.profileImg, datas.bookmark, datas.name, datas.phone)
         );
     });
-}
+};
 
-function makeHideFriendList(list) {
+const makeHideFriendList = (list) => {
     // friendListObj -> list
     removeOtherprofileInnerHTML();
     defaultStar();
     setPageTitle("숨김 목록 보기");
+    friendList.hide.classList.add("hided");
     friendListObj.filter(IsHided).map((datas) => {
         friendList.otherprofile.insertAdjacentHTML(
             "beforeend",
             friendFrameTwo(datas.profileImg, datas.name, datas.phone, "숨김")
         );
     });
-}
+};
 
-function makeBlockFriendList(list) {
+const makeBlockFriendList = (list) => {
     // friendListObj -> list
     removeOtherprofileInnerHTML();
     defaultStar();
     setPageTitle("차단 목록 보기");
+    friendList.block.classList.add("blocked");
     friendListObj.filter(IsBlocked).map((datas) => {
         friendList.otherprofile.insertAdjacentHTML(
             "beforeend",
             friendFrameTwo(datas.profileImg, datas.name, datas.phone, "차단")
         );
     });
-}
+};
 
-function makeFriendList(list) {
+const makeFriendList = (list) => {
     // friendListObj -> list
     removeOtherprofileInnerHTML();
     setPageTitle("내 친구 보기");
@@ -210,73 +188,37 @@ function makeFriendList(list) {
             friendFrame(datas.profileImg, datas.bookmark, datas.name, datas.phone)
         );
     });
-}
+};
 
-function removeOtherprofileInnerHTML() {
-    friendList.otherprofile.innerHTML = "";
-}
-
-function showDetails(el) {
+const showDetails = (el) => {
     el.parentNode.childNodes[3].classList.add("showDetails");
-}
+};
 
-function defaultDetails() {
-    friendList.detailImg.forEach((el) => {
-        el.parentNode.childNodes[3].classList.remove("showDetails");
-    })
-}
-
-function defaultStar() {
-    let star = document.querySelector("#friendList_bookmark");
-    star.setAttribute("src", "../img/star.svg");
-}
-
-function setPageTitle(title) {
-    friendList.pageTitle.innerHTML = title;
-}
-
-function disShowDetails(el) {
+const disShowDetails = (el) => {
     el.parentNode.childNodes[3].classList.remove("showDetails");
-}
+};
 
-function IsHided(data) {
-    if(data.hide)
-        return true;
-    return false;
-}
-
-function IsBlocked(data) {
-    if (data.block)
-        return true;
-    return false;
-}
-
-function IsOnBookmark(data) {
-    if (data.bookmark)
-        return true;
-    return false;
-}
-
-function IsOnDetails(el) {
-    if (el.parentNode.childNodes[3].classList[1])
-        return true;
-    return false;
-}
-
-function detailImgInit(el) {
+const detailImgInit = (el) => {
     if (IsOnDetails(el)) {
         disShowDetails(el);
     } else {
         defaultDetails();
         showDetails(el);
     }
-}
+};
 
-function setBookmarkStar(path) {
+const setBookmarkStar = (path) => {
     friendList.bookmark.setAttribute("src", path);
-}
+};
 
-function bookmarkInit() {
+const iconInit = () => {
+    friendList.block.classList.remove("blocked");
+    friendList.hide.classList.remove("hided");
+    defaultStar();
+};
+
+const bookmarkInit = () => {
+    // iconInit();
     if (friendList.bookmark.getAttribute("src").split("/")[2] === "star.svg") {
         setBookmarkStar("../img/starYellow.svg");
         makeBookmarkFriendList();
@@ -284,11 +226,35 @@ function bookmarkInit() {
         setBookmarkStar("../img/star.svg");
         makeFriendList();
     }
-}
+};
+
+const blockInit = () => {
+    const list = friendList.block.classList;
+    // iconInit();
+    if (list[0] === "blocked") {
+        list.remove("blocked");
+        makeFriendList();
+    } else {
+        list.add("blocked");
+        makeBlockFriendList();
+    }
+};
+
+const hideInit = () => {
+    const list = friendList.hide.classList;
+    // iconInit();
+    if (list[0] === "hided") {
+        list.remove("hided");
+        makeFriendList();
+    } else {
+        list.add("hided");
+        makeHideFriendList();
+    }
+};
 
 window.onload = () => {
     makeFriendList();
     friendList.detailImg = document.querySelectorAll(".friendList_details > img");
     friendList.bookmark = document.querySelector("#friendList_bookmark");
     friendList.addfriend = document.querySelector("#friendList_addfriend");
-}
+};
