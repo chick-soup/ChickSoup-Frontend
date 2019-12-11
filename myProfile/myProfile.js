bd8cb67251dab92654957181e8c1fb5097aff2ea
이성진 <chlskatnr1@naver.com>
2019-12-11 23:25:49 +0900

    [ADD] <myProfile.js> background 수정 기능 추가
    background 변경 버튼 -> 파일 고르고 수정 가능하도록 기능 추가

--- myProfile/myProfile.js
+++ myProfile/myProfile.js
@@ -41,7 +41,7 @@ const editFrame = (id, name, message) => {
         <label for="editprofile_profilephoto">프로필 사진 변경</label>
         <input type="file" onchange="changeImageFile()" id="editprofile_profilephoto">
         <label for="editprofile_backphoto">배경 사진 변경</label>
-        <input type="file" id="editprofile_backphoto">
+        <input type="file" onchange="changeBackImageFile()" id="editprofile_backphoto">
     </div>
     <div id="editprofile_change_info">
         <input type="text" id="editprofile_change_name" placeholder="이름" value="${name}">
@@ -73,20 +73,22 @@ const checkNicknameLength = () => {
 };
 
 const editMyProfile = () => {
-    const file = profileObj.file.files[0];
     let fd = new FormData();
     fd.append("nickname", profileObj.changeName.value.trim());
     fd.append("status_message", profileObj.changeMessage.value.trim());
-    if (sessionStorage.getItem("chicksoup_profile")) {
-        fd.append("profile", file);
-    }
+    fd.append("where", "web")
+    if (sessionStorage.getItem("chicksoup_profile"))
+        fd.append("profile", profileObj.file.files[0]);
+    if (sessionStorage.getItem("chicksoup_profileBack"))
+        fd.append("background", profileObj.backFile.files[0]);
+
     axios.put(`${server}/users/my/profile`, fd, {
         headers: {
             "Authorization": localStorage.getItem("access_token"),
             "Content-Type": "multipart/form-data",
         }
     })
-    // location.reload();
+    location.reload();
 };
 
 const changeImageFile = () => {
@@ -99,29 +101,52 @@ const changeImageFile = () => {
         return alert("파일 형식은 png, jpg, jpeg만 허용합니다. ");
     }
     let reader = new FileReader();
+    reader.onloadend = (e) => profileObj.photo.setAttribute("src", e.target.result);
+    reader.readAsDataURL(file);
+    // ? For editMyProfile
+    sessionStorage.setItem("chicksoup_profile", true);
+};
+
+const changeBackImageFile = () => {
+    const file = profileObj.backFile.files[0];
+    const fileSplit = file.name.split('.');
+    const fileExtension = fileSplit[fileSplit.length - 1];
+    if (!(fileExtension === "png"
+        || fileExtension === "jpg"
+        || fileExtension === "jpeg")) {
+        return alert("파일 형식은 png, jpg, jpeg만 허용합니다. ");
+    }
+    let reader = new FileReader();
     reader.onloadend = (e) => {
-        profileObj.photo.setAttribute("src", e.target.result);
+        profileObj.backImg.style.backgroundImage = `url("${e.target.result}")`
     }
     reader.readAsDataURL(file);
-    // ? convertImageFileAsFormData를 사용하기 위해서 
-    sessionStorage.setItem("chicksoup_profile", true);
+    // ? For editMyProfile
+    sessionStorage.setItem("chicksoup_profileBack", true);
 };
 
 const setUserImg = (id) => {
     profileObj.content.innerHTML = `<img src="https://chicksoup.s3.ap-northeast-2.amazonaws.com/media/image/user/profile/${id}.png" id="editprofile_userImg" alt="userImage">`;
 };
 
+const setUserBackImg = (id) => {
+    profileObj.backImg.style.backgroundImage = `url("${S3HOST}/media/image/user/background/web/${id}.png")`
+}
+
 const setMyprofile = (id, name, message) => {
     setUserImg(id);
+    setUserBackImg(id);
     profileObj.content.insertAdjacentHTML("beforeend", myFrame(id, name, message));
 };
 
 const setEditprofile = (id, name, message) => {
     sessionStorage.removeItem("chicksoup_profile");
+    sessionStorage.removeItem("chicksoup_profileBack");
     setUserImg(id);
     profileObj.content.insertAdjacentHTML("beforeend", editFrame(id, name, message));
     profileObj.changeMessage = document.querySelector("#editprofile_change_message");
     profileObj.changeName = document.querySelector("#editprofile_change_name");
+    profileObj.backFile = document.querySelector("#editprofile_backphoto");
     profileObj.file = document.querySelector("#editprofile_profilephoto");
     profileObj.photo = document.querySelector("#editprofile_userImg");
     profileObj.errorText = document.querySelector(".error_text");
@@ -142,9 +167,9 @@ const getUserInfo = () => {
             }
         }).catch((error) => {
             const state = error.response.status;
-            if(state === 470) {
+            if (state === 470) {
                 alert("볼 수 없는 프로필 입니다.");
-            } else if(state === 404) {
+            } else if (state === 404) {
                 alert("존재하지 않는 유저입니다.");
             }
             location.href = "../friendList/friendList.html";
@@ -155,8 +180,7 @@ const getUserInfo = () => {
 const getUserBackground = () => {
     const url = "/users/my/profile";
     axiosGETWithToken(url).then((datas) => {
-        const data = datas.data;
-        profileObj.backImg.style.backgroundImage = `url("${S3HOST}/media/image/user/background/web/${data.id}.png")`
+        setUserBackImg(datas.data.id);
     })
 };
 
